8fc1f58e71ec8870764cb7dffab57fb65bd0bc48
Kalonji Bankole <kkbankol@us.ibm.com>
2018-05-10 14:28:59 -0400

    adding dockerfile, setup script

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM ubuntu:16.04
+RUN apt-get update -y && \
+    apt-get install -y git curl python build-essential
+RUN git clone https://github.com/IBM/monitoring_ui
+RUN cd monitoring_ui && ./setup.sh
+EXPOSE 8081
+WORKDIR monitoring_ui
+CMD npm run dev-server
---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# apt-get update -y
+# apt-get install -y git curl
+# install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+# add nvm values to .bash_profile
+cat .nvm_profile >> ~/.bash_profile
+. ~/.bash_profile
+nvm install 8.9.0
+#nvm use 8.9.0
+npm install
+npm install --prefix react-backend/
